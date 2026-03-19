f9d1b859882e8b2616c2c63a676457ecbdbd575a
Exequiel <exequiel.miranda245@gmail.com>
2026-03-19 12:07:00 -0600

    initial commit

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import productRoutes from "./src/routes/products.js"
 import faqsRoutes from "./src/routes/faqs.js"
+import branchesRoutes from "./src/routes/branches.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json())
 
 app.use("/api/products", productRoutes)
 app.use("/api/faqs", faqsRoutes)
+app.use("/api/branches", branchesRoutes)
 
 
 export default app;
